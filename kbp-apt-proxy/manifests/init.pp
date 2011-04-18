1bcc4670d69e7584dca5c031f236a5d6807489d1
Rutger Spiertz <rutger@kumina.nl>
2011-04-18 15:07:59 +0200

    Update file to kfile.

--- kbp-apt-proxy/manifests/init.pp
+++ kbp-apt-proxy/manifests/init.pp
@@ -1,5 +1,5 @@
 class kbp-apt-proxy inherits approx {
-	File["/etc/approx/approx.conf"] {
-		source => "puppet://puppet/kbp-apt-proxy/approx/approx.conf",
+	Kfile["/etc/approx/approx.conf"] {
+		source => "kbp-apt-proxy/approx/approx.conf",
 	}
 }
