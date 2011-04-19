69210cf6aaa2277e11ad732e6652a5cbec938fff
Tim Stoop <tim@kumina.nl>
2011-04-19 12:12:24 +0200

    Change vim to new setup.

--- kbp_vim/manifests/init.pp
+++ kbp_vim/manifests/init.pp
@@ -14,35 +14,35 @@ class kbp_vim {
 			require => Global_vim_setting['silent execute "!mkdir -p ~/.tmp"'];
 		'silent execute "!mkdir -p ~/.tmp"':;
 	}
-
-	define global_vim_setting {
-		line { "global vim setting ${name}":
-			ensure  => "present",
-			file    => "/etc/vim/vimrc",
-			content => "${name}",
-		}
-	}
-
-	define vim_addon($package=false) {
-		# Install and activate a vim addon. Use as follows:
-		# kbp_vim::vim_addon { "puppet": package => "vim-puppet"; }
-		include kbp_vim::addon-manager
-		$the_package = $package ? {
-			false   => $name,
-			default => $package,
-		}
-		kpackage { $the_package:
-			ensure => latest;
-		}
-		exec { "/usr/bin/vim-addons -w install ${name}":
-			unless  => "/usr/bin/vim-addons -w -q show ${name} | grep 'installed' 2>&1 > /dev/null",
-			require => Package["vim-addon-manager", "${the_package}"];
-		}
-	}
 }
 
 class kbp_vim::addon-manager {
 	kpackage { "vim-addon-manager":
 		ensure => "latest";
+	}
+}
+
+define global_vim_setting {
+	line { "global vim setting ${name}":
+		ensure  => "present",
+		file    => "/etc/vim/vimrc",
+		content => "${name}",
+	}
+}
+
+define vim_addon($package=false) {
+	# Install and activate a vim addon. Use as follows:
+	# kbp_vim::vim_addon { "puppet": package => "vim-puppet"; }
+	include kbp_vim::addon-manager
+	$the_package = $package ? {
+		false   => $name,
+		default => $package,
+	}
+	kpackage { $the_package:
+		ensure => latest;
+	}
+	exec { "/usr/bin/vim-addons -w install ${name}":
+		unless  => "/usr/bin/vim-addons -w -q show ${name} | grep 'installed' 2>&1 > /dev/null",
+		require => Package["vim-addon-manager", "${the_package}"];
 	}
 }
