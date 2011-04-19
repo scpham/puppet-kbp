19cd1c0f4ffb5224a3d8936edfe4db70bbc8ef25
Tim Stoop <tim@kumina.nl>
2011-04-19 12:24:53 +0200

    Move most of this to gen_vim, where it belongs.

--- kbp_vim/manifests/init.pp
+++ kbp_vim/manifests/init.pp
@@ -1,5 +1,7 @@
 class kbp_vim {
-	global_vim_setting {
+	include gen_vim
+
+	gen_vim::global_setting {
 		"syntax on":;
 		"set ai":;
 		"set ts=8":;
@@ -13,36 +15,5 @@ class kbp_vim {
 		"set directory=~/.tmp/":
 			require => Global_vim_setting['silent execute "!mkdir -p ~/.tmp"'];
 		'silent execute "!mkdir -p ~/.tmp"':;
-	}
-}
-
-class kbp_vim::addon-manager {
-	kpackage { "vim-addon-manager":
-		ensure => "latest";
-	}
-}
-
-define global_vim_setting {
-	line { "global vim setting ${name}":
-		ensure  => "present",
-		file    => "/etc/vim/vimrc",
-		content => "${name}",
-	}
-}
-
-define vim_addon($package=false) {
-	# Install and activate a vim addon. Use as follows:
-	# kbp_vim::vim_addon { "puppet": package => "vim-puppet"; }
-	include kbp_vim::addon-manager
-	$the_package = $package ? {
-		false   => $name,
-		default => $package,
-	}
-	kpackage { $the_package:
-		ensure => latest;
-	}
-	exec { "/usr/bin/vim-addons -w install ${name}":
-		unless  => "/usr/bin/vim-addons -w -q show ${name} | grep 'installed' 2>&1 > /dev/null",
-		require => Package["vim-addon-manager", "${the_package}"];
 	}
 }
