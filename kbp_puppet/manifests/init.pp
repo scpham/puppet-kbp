4654754ab5405d8a5954937c7fbc0458964d4409
Tim Stoop <tim@kumina.nl>
2011-04-20 16:58:24 +0200

    Make sure we get the puppet config from the right place.

--- kbp_puppet/manifests/init.pp
+++ kbp_puppet/manifests/init.pp
@@ -2,6 +2,19 @@ class kbp_puppet {
 	include gen_puppet
 
 	gen_apt::preference { ["puppet","puppet-common"]:; }
+
+	# Default config for all our puppet clients
+	gen_puppet::set_config {
+		"logdir":      value => '/var/log/puppet';
+		"vardir":      value => '/var/lib/puppet';
+		"ssldir":      value => '/var/lib/puppet/ssl';
+		"rundir":      value => '/var/run/puppet';
+		# Single quotes in the next resources prevent them being expanded
+		"factpath":    value => '$vardir/lib/facter';
+		"templatedir": value => '$confdir/templates';
+		"pluginsync":  value => 'true';
+		"environment": value => $environment;
+	}
 }
 
 class kbp_puppet::vim {
