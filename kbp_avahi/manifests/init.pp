2f83b588514d8292709161122cff13c3f5d68a2a
Rutger Spiertz <rutger@kumina.nl>
2011-04-27 14:22:22 +0200

    Remove unneeded param.

--- kbp_avahi/manifests/init.pp
+++ kbp_avahi/manifests/init.pp
@@ -2,7 +2,6 @@ class kbp_avahi::daemon {
 	include avahi::daemon
 
 	ferm::new::rule { "MDNS traffic":
-		interface => "vlan7",
 		proto     => "udp",
 		dport     => "5353",
 		daddr     => "244.0.0.251",
