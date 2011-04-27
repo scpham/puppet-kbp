ac3c63a439a753a75d97de8642d3b2e6be848930
Rutger Spiertz <rutger@kumina.nl>
2011-04-27 15:14:33 +0200

    Fix typo.

--- kbp_avahi/manifests/init.pp
+++ kbp_avahi/manifests/init.pp
@@ -1,7 +1,7 @@
 class kbp_avahi::daemon {
 	include avahi::daemon
 
-	ferm::new::rule { "MDNS traffic,_v46":
+	ferm::new::rule { "MDNS traffic_v46":
 		proto     => "udp",
 		dport     => "5353",
 		daddr     => "244.0.0.251",
