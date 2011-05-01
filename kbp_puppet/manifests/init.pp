6c635b55797cdd045765a071fb84d1d5eaeaa14b
Tim Stoop <tim@kumina.nl>
2011-05-01 17:11:00 +0200

    Make sure we do not break clients once we merge to master.

--- kbp_puppet/manifests/init.pp
+++ kbp_puppet/manifests/init.pp
@@ -2,6 +2,10 @@ class kbp_puppet {
 	include gen_puppet
 
 	gen_apt::preference { ["puppet","puppet-common"]:; }
+}
+
+class kbp_puppet::test_default_config {
+	include gen_puppet::puppet_conf
 
 	# Default config for all our puppet clients
 	gen_puppet::set_config {
