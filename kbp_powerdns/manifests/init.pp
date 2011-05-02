8de6703f8ea41aafadcb71c22765761978251fa4
Rutger Spiertz <rutger@kumina.nl>
2011-05-02 14:49:55 +0200

    Import poweradmin ferm rules.

--- kbp_powerdns/manifests/init.pp
+++ kbp_powerdns/manifests/init.pp
@@ -2,4 +2,5 @@ class kbp_powerdns::master {
 	include powerdns::master
 
 	Ferm::New::Rule <<| tag == "ferm_bind_rule_${environment}" |>>
+	Ferm::New::Rule <<| tag == "ferm_poweradmin_rule_${environment}" |>>
 }
