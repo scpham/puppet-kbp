e76c1bce994d96ad195a7ddd2c91ecee76de985f
Rutger Spiertz <rutger@kumina.nl>
2011-05-03 13:07:29 +0200

    Add ferm export for logging purposes.

--- kbp_asterisk/manifests/init.pp
+++ kbp_asterisk/manifests/init.pp
@@ -6,4 +6,12 @@ class kbp_asterisk::server {
 		dport  => "(sip 15000:15100)",
 		action => "ACCEPT";
 	}
+
+	@@ferm::new::rule { "Asterisk CDR logging from ${fqdn}_v4":
+		saddr  => "81.30.39.28",
+		proto  => "tcp",
+		dport  => 3306,
+		action => "ACCEPT",
+		tag    => "ferm_mysql_rule_asterisk";
+	}
 }
