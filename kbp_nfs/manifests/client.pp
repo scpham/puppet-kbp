2fc1355a591e140db0304ad1609242252d923725
Tim Stoop <tim@kumina.nl>
2011-05-09 11:51:01 +0200

    The file is called excludes, plural.

--- kbp_nfs/manifests/client.pp
+++ kbp_nfs/manifests/client.pp
@@ -1,5 +1,6 @@
 class kbp_nfs::client {
 	include gen_nfs::client
+	include kbp_trending::client::nfs
 }
 
 class kbp_nfs::client::trending::munin {
@@ -7,13 +8,15 @@ class kbp_nfs::client::trending::munin {
 }
 
 define kbp_nfs::client::mount ($source) {
+	include kbp_nfs::client
+
 	gen_nfs::client::mount { $name:
 		source => $source,
 	}
 
 	# Exclude this mount from the backups
 	line { "Exclude NFS mount ${name} from backups.":
-		file    => "/etc/backup/exclude",
+		file    => "/etc/backup/excludes",
 		content => "${name}",
 		require => Kpackage["offsite-backup"],
 	}
