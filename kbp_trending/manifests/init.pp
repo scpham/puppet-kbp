29eeaf006779b1a816f9554adef233524acd20e4
Tim Stoop <tim@kumina.nl>
2011-05-09 11:43:20 +0200

    Fail instead of err.

--- kbp_trending/manifests/init.pp
+++ kbp_trending/manifests/init.pp
@@ -4,20 +4,20 @@ class kbp_trending {
 class kbp_trending::puppetmaster ($method="munin") {
 	case $method {
 		"munin": { include kbp_munin::client::puppetmaster }
-		default: { err("No trending for ${method}.") }
+		default: { fail("No trending for ${method}.") }
 	}
 }
 
 class kbp_trending::mysql ($method="munin") {
 	case $method {
 		"munin": { include kbp_munin::client::mysql }
-		default: { err("No trending for ${method}.") }
+		default: { fail("No trending for ${method}.") }
 	}
 }
 
 class kbp_trending::nfs ($method="munin") {
 	case $method {
 		"munin": { include kbp_munin::client::nfs }
-		default: { err("No trending for ${method}.") }
+		default: { fail("No trending for ${method}.") }
 	}
 }
