c0b15df9de4287b8f9b0eae293651bda07f0ff6a
Tim Stoop <tim@kumina.nl>
2011-05-10 16:34:11 +0200

    Add mountd port.

--- kbp_nfs/manifests/server.pp
+++ kbp_nfs/manifests/server.pp
@@ -10,13 +10,14 @@ class kbp_nfs::server::default_config {
 	gen_nfs::server::config { "dummy for nfs server":
 		incoming_port => "4000",
 		outgoing_port => "4001",
+		mountd_port   => "4002",
 		rpcmountdopts => "--state-directory-path /srv/nfs",
 		statdopts     => "--state-directory-path /srv/nfs",
 	}
 
 	ferm::new::rule { "Ports for nfsd":
 		proto  => "(tcp udp)",
-		dport  => "(4000 4001)",
+		dport  => "(4000 4001 4002)",
 		action => "ACCEPT";
 	}
 }
