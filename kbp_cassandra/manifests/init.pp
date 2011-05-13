a9fb6ea7b614ab94fe32cf01ab1ff3625903b6a8
Rutger Spiertz <rutger@kumina.nl>
2011-05-13 10:52:55 +0200

    Fix ferm import tag.

--- kbp_cassandra/manifests/init.pp
+++ kbp_cassandra/manifests/init.pp
@@ -1,5 +1,5 @@
 class kbp_cassandra::server {
-	Ferm::Rule <<| tag == "ferm_cassandra_rule_JMX_${environment}" |>>
+	Ferm::Rule <<| tag == "ferm_cassandra_rule_jmx_${environment}" |>>
 	Ferm::Rule <<| tag == "ferm_cassandra_rule_cluster_${environment}" |>>
 	Ferm::Rule <<| tag == "ferm_cassandra_rule_client_${environment}" |>>
 	Ferm::Rule <<| tag == "ferm_cassandra_rule_monitoring" |>>
