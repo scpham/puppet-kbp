9e92fa798727f4d40961232b88f0526288845f43
Rutger Spiertz <rutger@kumina.nl>
2011-05-13 13:32:44 +0200

    Remove incorrect import.

--- kbp_cassandra/manifests/init.pp
+++ kbp_cassandra/manifests/init.pp
@@ -10,6 +10,5 @@ class kbp_cassandra::client {
 
 class kbp_cassandra::server {
 	Ferm::Rule <<| tag == "ferm_cassandra_${environment}" |>>
-	Ferm::Rule <<| tag == "ferm_cassandra_${environment}_stage" |>>
 	Ferm::Rule <<| tag == "ferm_cassandra_monitoring" |>>
 }
