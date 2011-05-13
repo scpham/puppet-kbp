3dad8539bd9441b80a370af1e3bf9541f06ff2ce
Rutger Spiertz <rutger@kumina.nl>
2011-05-13 09:37:29 +0200

    Add monitoring ferm imports.

--- kbp_cassandra/manifests/init.pp
+++ kbp_cassandra/manifests/init.pp
@@ -2,4 +2,5 @@ class kbp_cassandra::server {
 	Ferm::New::Rule <<| tag == "ferm_cassandra_rule_JMX_${environment}" |>>
 	Ferm::New::Rule <<| tag == "ferm_cassandra_rule_cluster_${environment}" |>>
 	Ferm::New::Rule <<| tag == "ferm_cassandra_rule_client_${environment}" |>>
+	Ferm::New::Rule <<| tag == "ferm_cassandra_rule_monitoring" |>>
 }
