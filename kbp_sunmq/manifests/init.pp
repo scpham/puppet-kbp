5993d8047d915d639d971b8de80f4c83a8cece65
Rutger Spiertz <rutger@kumina.nl>
2011-05-17 12:19:43 +0200

    Add JMS port.

--- kbp_sunmq/manifests/init.pp
+++ kbp_sunmq/manifests/init.pp
@@ -1,8 +1,14 @@
 class kbp_sunmq {
-	ferm::rule { "SunMQ ports":
-		proto  => "tcp",
-		dport  => "(7676 10236)",
-		action => "ACCEPT",
-		tag    => "sunmq";
+	ferm::rule {
+		"SunMQ ports":
+			proto  => "tcp",
+			dport  => "(7676 10236)",
+			action => "ACCEPT",
+			tag    => "sunmq_${environment}";
+		"JMS port":
+			proto  => "tcp",
+			dport  => "10234",
+			action => "ACCEPT",
+			tag    => "sunmq_${environment}";
 	}
 }
