b5b0d95ca648958624e370882913a20d57c35dd4
Rutger Spiertz <rutger@kumina.nl>
2011-05-17 12:25:03 +0200

    Make JMS port optional.

--- kbp_sunmq/manifests/init.pp
+++ kbp_sunmq/manifests/init.pp
@@ -1,14 +1,17 @@
-class kbp_sunmq {
-	ferm::rule {
-		"SunMQ ports":
-			proto  => "tcp",
-			dport  => "(7676 10236)",
-			action => "ACCEPT",
-			tag    => "sunmq";
-		"JMS port":
+class kbp_sunmq($withJMS=false) {
+	ferm::rule { "SunMQ ports":
+		proto  => "tcp",
+		dport  => "(7676 10236)",
+		action => "ACCEPT",
+		tag    => "sunmq";
+	}
+	
+	if $withJMS {
+		ferm::rule { "JMS port":
 			proto  => "tcp",
 			dport  => "10234",
 			action => "ACCEPT",
 			tag    => "sunmq";
+		}
 	}
 }
