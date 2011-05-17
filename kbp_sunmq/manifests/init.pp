0467c02009c13b1380b28ab77b141d871602124d
Rutger Spiertz <rutger@kumina.nl>
2011-05-17 12:21:27 +0200

    Adding environment to a tag is not needed with local resources.

--- kbp_sunmq/manifests/init.pp
+++ kbp_sunmq/manifests/init.pp
@@ -4,11 +4,11 @@ class kbp_sunmq {
 			proto  => "tcp",
 			dport  => "(7676 10236)",
 			action => "ACCEPT",
-			tag    => "sunmq_${environment}";
+			tag    => "sunmq";
 		"JMS port":
 			proto  => "tcp",
 			dport  => "10234",
 			action => "ACCEPT",
-			tag    => "sunmq_${environment}";
+			tag    => "sunmq";
 	}
 }
