72e0efdf3b3b858c7cba8e3cebdfddc30143eb8d
Rutger Spiertz <rutger@kumina.nl>
2011-05-18 11:53:14 +0200

    Add internal ferm rules.

--- kbp_cassandra/manifests/init.pp
+++ kbp_cassandra/manifests/init.pp
@@ -8,7 +8,14 @@ class kbp_cassandra::client($customtag="cassandra_${environment}") {
 	}
 }
 
-class kbp_cassandra::server {
+class kbp_cassandra::server($customtag="cassandra_${environment}") {
+	@@ferm::rule { "Internal Cassandra connections from ${fqdn}":
+		saddr  => $fqdn,
+		proto  => "tcp",
+		dport  => 7000,
+		action => "ACCEPT",
+		tag    => $customtag;
+	}
 	Ferm::Rule <<| tag == "cassandra_${environment}" |>>
 	Ferm::Rule <<| tag == "cassandra_monitoring" |>>
 }
