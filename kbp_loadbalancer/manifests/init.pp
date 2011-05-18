53b415acaacd5908a18a871ea376d1d233fa2cb2
Rutger Spiertz <rutger@kumina.nl>
2011-05-18 10:54:43 +0200

    Fix param name.

--- kbp_loadbalancer/manifests/init.pp
+++ kbp_loadbalancer/manifests/init.pp
@@ -1,8 +1,8 @@
 define kbp_loadbalancer::site ($listenaddress, $port=80, $sslport=false, monitoring=true) {
 	kbp_haproxy::site { "${name}":
-		address    => $listenaddress,
-		port       => $port,
-		monitoring => $monitoring;
+		listenaddress => $listenaddress,
+		port          => $port,
+		monitoring    => $monitoring;
 	}
 
 	if $sslport {
