47e1ba8bcfe777d9195d25f2afce1740a3f90f8f
Rutger Spiertz <rutger@kumina.nl>
2011-06-03 16:44:32 +0200

    Add remote ntp check servercommand.

--- kbp_icinga/manifests/init.pp
+++ kbp_icinga/manifests/init.pp
@@ -41,6 +41,11 @@ class kbp_icinga::client {
 			service_description => "Open files",
 			checkcommand        => "check_open_files",
 			nrpe                => true;
+		"ntp_offset_${fqdn}":
+			service_description => "NTP offset",
+			servicegroups       => "mail_services",
+			checkcommand        => "check_remote_ntp",
+			nrpe                => true;
 		"ntpd_${fqdn}":
 			service_description => "NTPD",
 			servicegroups       => "mail_services",
@@ -90,7 +95,7 @@ class kbp_icinga::server {
 	gen_icinga::servercommand {
 		["check_ssh","check_smtp"]:
 			conf_dir => "generic";
-		["check_open_files","check_cpu","check_disk_space","check_ksplice","check_memory","check_puppet_state_freshness","check_zombie_processes","check_local_smtp","check_drbd","check_pacemaker","check_mysql","check_mysql_slave","check_loadtrend","check_heartbeat","check_ntpd"]:
+		["check_open_files","check_cpu","check_disk_space","check_ksplice","check_memory","check_puppet_state_freshness","check_zombie_processes","check_local_smtp","check_drbd","check_pacemaker","check_mysql","check_mysql_slave","check_loadtrend","check_heartbeat","check_ntpd","check_remote_ntp"]:
 			conf_dir => "generic",
 			nrpe     => true;
 		"return-ok":
