e3ddd618bf514569886f9b4037daba0ae4e71eb8
Rutger Spiertz <rutger@kumina.nl>
2011-06-20 15:44:49 +0200

    Fix typo.

--- kbp_monitoring/manifests/init.pp
+++ kbp_monitoring/manifests/init.pp
@@ -116,7 +116,7 @@ define kbp_monitoring::site($package="icinga", $address=false, $conf_dir=$false,
 			kbp_icinga::site { "${name}":
 				address  => $address ? {
 					false   => undef,
-					default => $adddress,
+					default => $address,
 				},
 				conf_dir => $conf_dir ? {
 					false   => undef,
