d779b7925c01cfca6220b16b8cd7e8c4540137d0
Tim Stoop <tim@kumina.nl>
2011-07-11 23:43:30 +0200

    Add the function to create repositories.

--- kbp_git/manifests/init.pp
+++ kbp_git/manifests/init.pp
@@ -12,3 +12,33 @@
 class kbp_git {
 	include gen_git
 }
+
+# Define: kbp_git::repo
+#
+# Actions:
+#»˙˙˙˙˙˙Set up git repository
+#
+# Parameters:
+#»˙˙˙˙˙˙name
+#»˙˙˙˙˙˙»˙˙˙˙˙˙˙The directory where to create the repository.
+#»˙˙˙˙˙˙»˙˙˙˙˙˙˙Needs to be a kfile already.
+#»˙˙˙˙˙˙branch
+#»˙˙˙˙˙˙»˙˙˙˙˙˙˙The remote branch of the origin. Defaults to
+#»˙˙˙˙˙˙»˙˙˙˙˙˙˙"master".
+#»˙˙˙˙˙˙origin
+#»˙˙˙˙˙˙»˙˙˙˙˙˙˙Add an origin to the repository. This does
+#»˙˙˙˙˙˙»˙˙˙˙˙˙˙not clone the remote repository.
+#
+# Depends:
+#»˙˙˙˙˙˙kbp_git
+#	kbp_git::repo
+#»˙˙˙˙˙˙gen_puppet
+#
+define kbp_git::repo ($branch = "master", $origin = false) {
+	include kbp_git
+
+	gen_git::repo { $name:
+		branch => $branch,
+		origin => $origin,
+	}
+}
