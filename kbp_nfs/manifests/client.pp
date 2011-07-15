e9e93fb7a9f831728bbd0b48c4dc7e82b4dcd849
Rutger Spiertz <rutger@kumina.nl>
2011-07-15 11:32:03 +0200

    Update to the new gen_nfs.

--- kbp_nfs/manifests/client.pp
+++ kbp_nfs/manifests/client.pp
@@ -10,7 +10,6 @@
 #	gen_puppet
 #
 class kbp_nfs::client {
-	include gen_nfs::client
 	include kbp_trending::nfs
 
 	@@gen_ferm::rule { "NFS connections from ${fqdn}":
@@ -37,20 +36,18 @@ class kbp_nfs::client {
 define kbp_nfs::client::mount ($source) {
 	include kbp_nfs::client
 
-	gen_nfs::client::mount { $name:
-		source => $source,
+	gen_nfs::mount { $name:
+		source => $source;
 	}
 
-	# Exclude this mount from the backups
 	line { "Exclude NFS mount ${name} from backups.":
 		file    => "/etc/backup/excludes",
-		content => "${name}",
-		require => Kpackage["offsite-backup"],
+		content => $name,
+		require => Kpackage["offsite-backup"];
 	}
 
-	# Check if the mount is still available, if not, remount
 	exec { "/bin/mount -o remount ${name}":
 		unless  => "/bin/sh -c 'cd ${name}'",
-		require => Gen_nfs::Client::Mount["${name}"],
+		require => Gen_nfs::Mount["${name}"];
 	}
 }
--- kbp_nfs/manifests/server.pp
+++ kbp_nfs/manifests/server.pp
@@ -13,60 +13,20 @@
 #	Undocumented
 #	gen_puppet
 #
-class kbp_nfs::server ($default_config = true) {
-	include gen_nfs::server
+class kbp_nfs::server($need_gssd = "no", $need_idmapd = "no", $need_statd = "yes",
+		$need_svcgssd = "no", $mountd_port = false, $incoming_port = false,
+		$outgoing_port = false, $lock_port = false, $rpcnfsdcount = "8",
+		$rpcnfsdpriority = "0", $rpcmountdopts = "", $rpcsvcgssdopts = "",
+		$statdopts = "") {
 	include kbp_trending::nfsd
 	include kbp_monitoring::nfs
-
-	if $default_config {
-		# Use this for a default NFS server
-		kbp_nfs::server::config { "dummy for nfs server":
-			incoming_port => "4000",
-			outgoing_port => "4001",
-			mountd_port   => "4002",
-			lock_port     => "4003",
-			rpcmountdopts => "--state-directory-path /srv/nfs",
-			statdopts     => "--state-directory-path /srv/nfs",
-		}
-	}
-}
-
-# Define: kbp_nfs::server::config
-#
-# Parameters:
-#	need_idmapd
-#		Undocumented
-#	need_statd
-#		Undocumented
-#	need_gssd
-#		Undocumented
-#
-# Actions:
-#	Undocumented
-#
-# Depends:
-#	Undocumented
-#	gen_puppet
-#
-define kbp_nfs::server::config ($need_gssd = "no", $need_idmapd = "no", $need_statd = "yes",
-				$need_svcgssd = "no", $mountd_port = false, $incoming_port = false,
-				$outgoing_port = false, $lock_port = false, $rpcnfsdcount = "8",
-				$rpcnfsdpriority = "0", $rpcmountdopts = "", $rpcsvcgssdopts = "",
-				$statdopts = "") {
-	gen_nfs::server::config { $name:
-		need_gssd       => $need_gssd,
-		need_idmapd     => $need_idmapd,
-		need_statd      => $need_statd,
-		need_svcgssd    => $need_svcgssd,
-		mountd_port     => $mountd_port,
-		incoming_port   => $incoming_port,
-		outgoing_port   => $outgoing_port,
-		lock_port       => $lock_port,
-		rpcnfsdcount    => $rpcnfsdcount,
-		rpcnfsdpriority => $rpcnfsdpriority,
-		rpcmountdopts   => $rpcmountdopts,
-		rpcsvcgssdopts  => $rpcsvcgssdopts,
-		statdopts       => $statdopts,
+	class { "gen_nfs::server":
+		incoming_port => "4000",
+		outgoing_port => "4001",
+		mountd_port   => "4002",
+		lock_port     => "4003",
+		rpcmountdopts => "--state-directory-path /srv/nfs",
+		statdopts     => "--state-directory-path /srv/nfs";
 	}
 
 	Gen_ferm::Rule <<| tag == "nfs_${environment}" |>> {
