721d57916c451b2f7868eedcf5c24a7c912eece3
Tim Stoop <tim@kumina.nl>
2011-08-02 14:11:07 +0200

    Support for SSL.

--- kbp_rabbitmq/manifests/init.pp
+++ kbp_rabbitmq/manifests/init.pp
@@ -3,14 +3,28 @@
 # Class: kbp_rabbitmq
 #
 # Actions:
-#	Undocumented
+#	Setup a specific version of rabbitmq and deploy some config for it.
 #
 # Depends:
-#	Undocumented
+#	gen_rabbitmq
 #	gen_puppet
 #
-class kbp_rabbitmq($version) {
+class kbp_rabbitmq($version, $port = 5672, $ssl_cert = false, $ssl_key = false, $ssl_port = 5671) {
 	class { "gen_rabbitmq":
-		version => $version;
+		ssl_cert => $ssl_cert,
+		ssl_key  => $ssl_key,
+		ssl_port => $ssl_port,
+		version  => $version;
+	}
+
+	Gen_ferm::Rule <<| tag == "rabbitmq_${environment}" |>> {
+		dport => $port,
+	}
+
+	# Open the port for the clients
+	if $ssl_cert {
+		Gen_ferm::Rule <<| tag == "rabbitmq_ssl_${environment}" |>> {
+			dport => $ssl_port,
+		}
 	}
 }
