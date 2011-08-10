ba7399022e2e4d1d9ec50391ed242b460e227173
Rutger Spiertz <rutger@kumina.nl>
2011-08-10 11:47:26 +0200

    Allow for lower free mem perc on hosts.

--- kbp_icinga/manifests/init.pp
+++ kbp_icinga/manifests/init.pp
@@ -53,7 +53,10 @@ class kbp_icinga::client {
 		"check_mbean_value":
 			arguments => '$ARG1$ $ARG2$ $ARG3$ $ARG4$';
 		"check_memory":
-			arguments => "-w 6 -c 3";
+			arguments => $is_virtual ? {
+				true  => "-w 6 -c 3",
+				false => "-w 4 -c 2",
+			};
 		"check_mysql":
 			arguments => "-u nagios";
 		"check_mysql_slave":
