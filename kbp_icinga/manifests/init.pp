42bcb8f6e8dc47e0940e12303e3ab4b128bb693d
Rutger Spiertz <rutger@kumina.nl>
2011-08-22 14:54:08 +0200

    Change intervals for puppet failure check.

--- kbp_icinga/manifests/init.pp
+++ kbp_icinga/manifests/init.pp
@@ -122,8 +122,7 @@ class kbp_icinga::client {
 		"puppet_failures":
 			service_description => "Puppet failures",
 			check_command       => "check_puppet_failures",
-			check_interval      => 10800,
-			retry_interval      => 10800,
+			retry_interval      => 3600,
 			nrpe                => true,
 			sms                 => false;
 		"cpu":
