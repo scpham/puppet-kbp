fc288b0fd8604f98bdb83e60c8280e7941197e7d
Pieter Lexis <pieter@kumina.nl>
2011-10-11 15:49:39 +0200

    Lenny machines run puppet 2.7 now
    This reverts commit a71b290c1fcbc00d573c87ced7b23a00fc9ce03f.

--- kbp_ssh/manifests/init.pp
+++ kbp_ssh/manifests/init.pp
@@ -18,15 +18,13 @@ class kbp_ssh {
 		tag    => "ferm";
 	}
 
-
-# DISABLED SO LENNY BOXES CAN KICK
 	# Disable password logins and root logins
-#	augeas { "sshd_config":
-#		context => "/files/etc/ssh/sshd_config",
-#		changes => [
-#			"set PermitRootLogin no",
-#			"set PasswordAuthentication no"
-#		],
-#		notify  => Service["ssh"];
-#	}
+	augeas { "sshd_config":
+		context => "/files/etc/ssh/sshd_config",
+		changes => [
+			"set PermitRootLogin no",
+			"set PasswordAuthentication no"
+		],
+		notify  => Service["ssh"];
+	}
 }
