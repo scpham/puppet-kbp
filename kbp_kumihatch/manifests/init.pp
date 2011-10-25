2f245561d9a8db102611c9f65825abbec3062295
Pieter Lexis <pieter@kumina.nl>
2011-10-25 10:25:46 +0200

    change simple-cdd to kumihatch

--- kbp_kumihatch/manifests/init.pp
+++ kbp_kumihatch/manifests/init.pp
@@ -1,5 +1,4 @@
-class kbp_simple-cdd {
-	include gen_simple-cdd
+class kbp_kumihatch {
 	# Needed for converting PNG to a format that can be used as a spash screen
 	include gen_base::netpbm
 	include gen_base::bsdtar
