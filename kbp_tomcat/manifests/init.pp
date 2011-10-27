71f8253a74e4ebe8973d9535a9ddfc4eaad99be8
Tim Stoop <tim@kumina.nl>
2011-10-27 11:34:23 +0200

    Make stuff slightly prettier.

--- kbp_tomcat/manifests/init.pp
+++ kbp_tomcat/manifests/init.pp
@@ -17,7 +17,7 @@ class kbp_tomcat ($tomcat_tag="tomcat_${environment}", $domain=$fqdn, $serverali
 		jvm_max_mem          => $jvm_max_mem;
 	}
 
-	apache_proxy_ajp_site { "${domain}":
+	kbp_tomcat::apache_proxy_ajp_site { "${domain}":
 		ssl          => $ssl,
 		port         => $ajp13_connector_port,
 		serveralias  => $serveralias,
@@ -37,11 +37,29 @@ class kbp_tomcat ($tomcat_tag="tomcat_${environment}", $domain=$fqdn, $serverali
 	}
 }
 
+# Class: kbp_tomcat::mysql
+#
+# Actions:
+#	Undocumented
+#
+# Depends:
+#	Undocumented
+#	gen_puppet
+#
 class kbp_tomcat::mysql {
 	include kbp_tomcat
 	include kbp_mysql::client::java
 }
 
+# Define: kbp_tomcat::webapp
+#
+# Actions:
+#	Undocumented
+#
+# Depends:
+#	Undocumented
+#	gen_puppet
+#
 define kbp_tomcat::webapp($war="", $urlpath="/", $context_xml_content=false, $root_app=false) {
 	gen_tomcat::context { $name:
 		war                 => $war,
@@ -51,6 +69,15 @@ define kbp_tomcat::webapp($war="", $urlpath="/", $context_xml_content=false, $ro
 	}
 }
 
+# Define: kbp_tomcat::apache_proxy_ajp_site
+#
+# Actions:
+#	Undocumented
+#
+# Depends:
+#	Undocumented
+#	gen_puppet
+#
 define kbp_tomcat::apache_proxy_ajp_site($port, $ssl=false, $serveralias=false, $documentroot=false, $ensure="present", $tomcat_tag="tomcat_${environment}") {
 	apache::site_config { "${name}":
 		serveralias  => $serveralias,
@@ -64,12 +91,22 @@ define kbp_tomcat::apache_proxy_ajp_site($port, $ssl=false, $serveralias=false, 
 	}
 }
 
+# Define: kbp_tomcat::user
+#
+# Actions:
+#	Undocumented
+#
+# Depends:
+#	Undocumented
+#	gen_puppet
+#
 define kbp_tomcat::user ($username=false, $password, $role, $tomcat_tag="tomcat_${environment}") {
 	if !$username {
 		$the_username = $name
 	} else {
 		$the_username = $username
 	}
+
 	gen_tomcat::user { "${the_username}":
 		username   => $the_username,
 		password   => $password,
@@ -78,14 +115,24 @@ define kbp_tomcat::user ($username=false, $password, $role, $tomcat_tag="tomcat_
 	}
 }
 
+# Define: kbp_tomcat::role
+#
+# Actions:
+#	Undocumented
+#
+# Depends:
+#	Undocumented
+#	gen_puppet
+#
 define kbp_tomcat::role ($role=false, $tomcat_tag="tomcat_${environment}") {
 	if !$role {
 		$the_role = $name
 	} else {
 		$the_role = $role
 	}
+
 	gen_tomcat::role { "${the_role}":
-		role => $the_role,
+		role       => $the_role,
 		tomcat_tag =>  $tomcat_tag;
 	}
 }
