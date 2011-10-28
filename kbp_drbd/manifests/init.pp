efefc2586f3b8547a56ff041be77427980af1867
Rutger Spiertz <rutger@kumina.nl>
2011-10-28 09:46:38 +0200

    Set up ocfs2 if mastermaster setup is enabled.

--- kbp_drbd/manifests/init.pp
+++ kbp_drbd/manifests/init.pp
@@ -16,6 +16,11 @@
 define kbp_drbd($mastermaster=true, $time_out=false, $connect_int=false, $ping_int=false, $ping_timeout=false, $after_sb_0pri="discard-younger-primary",
 		$after_sb_1pri="discard-secondary", $after_sb_2pri="call-pri-lost-after-sb", $rate="5M") {
 	include kbp_drbd::monitoring::icinga
+	if $mastermaster {
+		class { "kbp_ocfs2":
+			ocfs2_tag => $name;
+		}
+	}
 
 	gen_drbd { $name:
 		mastermaster  => $mastermaster,
