18ecedd2a1e811088bff39e6fae3b8c5a8e1bf6e
Rutger Spiertz <rutger@kumina.nl>
2011-10-31 15:05:37 +0100

    Fix selector.

--- kbp_php5_xdebug/manifests/init.pp
+++ kbp_php5_xdebug/manifests/init.pp
@@ -1,13 +1,22 @@
-class gen_php5_xdebug {
+class kbp_php5_xdebug {
+	include gen_base::php5-common
+
+	if $lsbmajdistrelease > 5 {
+		$squeeze_or_newer = true
+	} else {
+		$squeeze_or_newer = false
+	}
+
 	kpackage { "php5-xdebug":
 		ensure => latest;
 	}
 
 	kfile { "/etc/php5/conf.d/xdebug.ini":
-		content => $lsbmajdistrelease > 5 ? {
+		content => $squeeze_or_newer ? {
 			true  => "zend_extension=/usr/lib/php5/20090626/xdebug.so\nxdebug.remote_enable=On\nhtml_errors=On\n",
 			false => "zend_extension=/usr/lib/php5/20060613/xdebug.so\nxdebug.remote_enable=On\nhtml_errors=On\n",
 		},
+		require => Package["php5-common"],
 		notify  => Exec["reload-apache2"];
 	}
 }
