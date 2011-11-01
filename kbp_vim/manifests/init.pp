a794bb8e21d8abad5dbc19a74a6f5881d0d85204
Tim Stoop <tim@kumina.nl>
2011-11-01 23:09:31 +0100

    Modify some settings for puppet files.

--- kbp_vim/manifests/init.pp
+++ kbp_vim/manifests/init.pp
@@ -3,42 +3,45 @@
 # Class: kbp_vim
 #
 # Actions:
-#	Undocumented
+#   Undocumented
 #
 # Depends:
-#	Undocumented
-#	gen_puppet
+#   Undocumented
+#   gen_puppet
 #
 class kbp_vim {
-	gen_vim::global_setting {
-		"syntax on":;
-		"set ai":;
-		"set ts=8":;
-		"set bg=dark":;
-		"set list":;
-		"set listchars=tab:»˙,trail:•":;
-		"set hlsearch":;
-		"set ruler":;
-		'silent execute "!mkdir -p ~/.tmp"':;
-		["set backupdir=~/.tmp/","set directory=~/.tmp/"]:
-			require => Gen_vim::Global_setting['silent execute "!mkdir -p ~/.tmp"'];
-		"autocmd FileType python set tabstop=4":;
-		"autocmd FileType python set shiftwidth=4":;
-		"autocmd FileType python set expandtab":;
-	}
+  gen_vim::global_setting {
+    "syntax on":;
+    "set ai":;
+    "set ts=8":;
+    "set bg=dark":;
+    "set list":;
+    "set listchars=tab:»˙,trail:•":;
+    "set hlsearch":;
+    "set ruler":;
+    "autocmd FileType puppet set textwidth=140":;
+    "autocmd FileType puppet set tabstop=2":;
+    "autocmd FileType puppet set expandtab":;
+    'silent execute "!mkdir -p ~/.tmp"':;
+    ["set backupdir=~/.tmp/","set directory=~/.tmp/"]:
+      require => Gen_vim::Global_setting['silent execute "!mkdir -p ~/.tmp"'];
+    "autocmd FileType python set tabstop=4":;
+    "autocmd FileType python set shiftwidth=4":;
+    "autocmd FileType python set expandtab":;
+  }
 }
 
 # Class: kbp_vim::puppet
 #
 # Actions:
-#	Undocumented
+#   Undocumented
 #
 # Depends:
-#	Undocumented
-#	gen_puppet
+#   Undocumented
+#   gen_puppet
 #
 class kbp_vim::puppet {
-	gen_vim::addon { "puppet":
-		package => "vim-puppet";
-	}
+  gen_vim::addon { "puppet":
+    package => "vim-puppet";
+  }
 }
