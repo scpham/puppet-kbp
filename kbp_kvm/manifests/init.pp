763da12635be93048c525420e87d4977a9684ef0
Tim Stoop <tim@kumina.nl>
2011-11-08 23:24:02 +0100

    Pinning is done in generic in this case.

--- kbp_kvm/manifests/init.pp
+++ kbp_kvm/manifests/init.pp
@@ -13,10 +13,6 @@
 class kbp_kvm {
   include gen_kvm
 
-  if $lsbmajdistrelease < 6 {
-    gen_apt::preference { "qemu-kvm":; }
-  }
-
   # Enable KSM
   exec { "/bin/echo 1 > /sys/kernel/mm/ksm/run":
     onlyif => "/usr/bin/test `cat /sys/kernel/mm/ksm/run` -eq 0",
