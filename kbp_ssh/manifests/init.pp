f24b75e0dc9531947d4faf0a5ab50f8c7be78e80
Tim Stoop <tim@kumina.nl>
2011-11-18 13:47:57 +0100

    Control sshd_config with augeas and revert previous commit about it. Also add a simply class that overrides permitrootlogin to allow forced commands.

--- kbp_ssh/manifests/init.pp
+++ kbp_ssh/manifests/init.pp
@@ -3,10 +3,11 @@
 # Class: kbp_ssh
 #
 # Actions:
-#  Undocumented
+#  Setup ssh the way we want it
 #
 # Depends:
-#  Undocumented
+#  kbp_openssl::common
+#  gen_ferm::rule
 #  gen_puppet
 #
 class kbp_ssh {
@@ -19,19 +20,32 @@ class kbp_ssh {
     tag    => "ferm";
   }
 
-  # remove this commit after 3 days....
-  kfile { "/etc/ssh/sshd_config":
-    source => "kbp_ssh/sshd_config";
-  }
-
   # Disable password logins and root logins
-  #augeas { "sshd_config":
-  #  lens    => 'Sshd.lns',
-  #  incl    => "/etc/ssh/sshd_config",
-  #  changes => [
-  #    "set PermitRootLogin forced-commands-only",
-  #    "set PasswordAuthentication no"
-  #  ],
-  #  notify  => Service["ssh"];
-  #}
+  kaugeas {
+    "sshd_config PermitRootLogin":
+      lens    => 'Sshd.lns',
+      file    => "/etc/ssh/sshd_config",
+      changes => "set PermitRootLogin no",
+      notify  => Service["ssh"];
+    "sshd_config PasswordAuthentication":
+      lens    => 'Sshd.lns',
+      file    => "/etc/ssh/sshd_config",
+      changes => "set PasswordAuthentication no",
+      notify  => Service["ssh"];
+  }
+}
+
+# Class: kbp_ssh::permit_root_logins_with_forced_commands
+#
+# Actions:
+#  Allow root logins with forced commands only
+#
+# Depends:
+#  kbp_ssh
+#  gen_puppet
+#
+class kbp_ssh::permit_root_logins_with_forced_commands {
+  Kaugeas <| title == "sshd_config PermitRootLogin" |> {
+    changes => "set PermitRootLogin forced-commands-only",
+  }
 }
