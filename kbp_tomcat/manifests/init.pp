9bf196e1e0aac47fb38f5b4e51f30c0abeab2aad
Tim Stoop <tim@kumina.nl>
2011-11-24 12:55:25 +0100

    Fix sudo rule.

--- kbp_tomcat/manifests/init.pp
+++ kbp_tomcat/manifests/init.pp
@@ -34,7 +34,7 @@ class kbp_tomcat ($tomcat_tag="tomcat_${environment}", $serveralias=false, $docu
   kbp_sudo::rule { "Allow tomcat manipulation when in the tomcat6 group":
     command           => "/etc/init.d/tomcat6 [a-z]*",
     as_user           => "root",
-    entity            => "@tomcat6",
+    entity            => "%tomcat6",
     password_required => false,
   }
 }
