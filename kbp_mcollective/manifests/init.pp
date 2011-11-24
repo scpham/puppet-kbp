26585871e046e995ea1ab13011843eb7d2f12bdc
Rutger Spiertz <rutger@kumina.nl>
2011-11-24 12:42:57 +0100

    Add kbp_mcollective and set up base and rabbitmq for it.

--- kbp_mcollective/manifests/init.pp
+++ kbp_mcollective/manifests/init.pp
@@ -1,3 +1,20 @@
-class kbp_mcollective {
-  include kbp_activemq
+class kbp_mcollective::server {
+  include gen_mcollective::server
+  class { "kbp_rabbitmq::client":
+    rabbitmq_name => "mcollective";
+  }
+
+  kfile { "/etc/mcollective/facts.yaml":
+    content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"),
+    require => Package["mcollective-common"];
+  }
+}
+
+class kbp_mcollective::client($pass_client, $pass_server) {
+  include gen_mcollective::client
+  class { "kbp_rabbitmq::mcollective":
+    pass_client   => $pass_client,
+    pass_server   => $pass_server,
+    rabbitmq_name => "mcollective";
+  }
 }
