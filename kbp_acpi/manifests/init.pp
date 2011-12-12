a7b189f51b4c3b258bf02b20f00467792b143334
Thomas Ronner <thomas@kumina.nl>
2011-12-12 11:30:14 +0100

    Pin package acpid to latest versions.

--- kbp_acpi/manifests/init.pp
+++ kbp_acpi/manifests/init.pp
@@ -11,4 +11,18 @@
 #
 class kbp_acpi {
   include gen_acpi
+
+  if $lsbmajdistrelease < 6 {
+    gen_apt::preference {
+      ["acpid"]:
+        version => "1.0.8-1lenny4";
+    }
+  }
+  else {
+    gen_apt::preference {
+      ["acpid"]:
+        version => "1:2.0.7-1squeeze3";
+    }
+  }
+
 }
