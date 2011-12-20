6fdc64fce5981c21609991b57e41e740d5337d45
Rutger Spiertz <rutger@kumina.nl>
2011-12-20 08:01:37 +0100

    Add defaults for statuscode.

--- kbp_haproxy/manifests/init.pp
+++ kbp_haproxy/manifests/init.pp
@@ -61,7 +61,7 @@ class kbp_haproxy ($failover = false, $haproxy_tag="haproxy_${environment}", $lo
 #  Undocumented
 #  gen_puppet
 #
-define kbp_haproxy::site ($listenaddress, $port=80, $monitor_site=true, $monitoring_ha=false, $monitoring_status=false, $monitoring_url=false, $monitoring_response=false, $monitoring_address=false, $cookie=false, $make_lbconfig, $httpcheck_uri=false, $httpcheck_port=false, $balance="static-rr", $max_check_attempts=false, $servername=$hostname, $serverip=$ipaddress_eth0, $serverport=80, $timeout_connect="15s", $timeout_server_client="20s", $timeout_http_request="10s", $tcp_sslport=false, $haproxy_tag="haproxy_${environment}") {
+define kbp_haproxy::site ($listenaddress, $port=80, $monitor_site=true, $monitoring_ha=false, $monitoring_status="200", $monitoring_url=false, $monitoring_response=false, $monitoring_address=false, $cookie=false, $make_lbconfig, $httpcheck_uri=false, $httpcheck_port=false, $balance="static-rr", $max_check_attempts=false, $servername=$hostname, $serverip=$ipaddress_eth0, $serverport=80, $timeout_connect="15s", $timeout_server_client="20s", $timeout_http_request="10s", $tcp_sslport=false, $haproxy_tag="haproxy_${environment}") {
   gen_ferm::rule { "HAProxy forward for ${name}":
     proto     => "tcp",
     daddr     => $listenaddress ? {
