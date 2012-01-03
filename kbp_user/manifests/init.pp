94030693fd3f005e40d91ebfccdf93c974c3424d
Rutger Spiertz <rutger@kumina.nl>
2012-01-03 11:41:17 +0100

    Add kbp_user define.

--- kbp_user/manifests/init.pp
+++ kbp_user/manifests/init.pp
@@ -2,6 +2,34 @@ class kbp_user::environment {
   Kbp_user::Hashfragment <<| tag == "generic_htpasswd" |>>
 }
 
+define kbp_user($uid, $gid, $comment, $groups=false, $keys=false) {
+  user { $name:
+    uid        => $uid,
+    gid        => $gid,
+    groups     => $groups ? {
+      false   => undef,
+      default => $groups,
+    },
+    comment    => $comment,
+    managehome => true,
+    require    => Group[$gid];
+  }
+
+  if $keys {
+    expand_keys { $keys:
+      user => $name;
+    }
+  }
+
+  define expand_keys {
+    ssh_authorized_key { "${user}_${comment}":
+      user => $user,
+      key  => $name,
+      type => "ssh-rsa";
+    }
+  }
+}
+
 define kbp_user::hashfragment($hash) {
   @@concat::add_content { "${hash}_${environment}":
     content => $hash,
