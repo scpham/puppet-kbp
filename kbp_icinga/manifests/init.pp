81dbe5cfc2b7b931b55d4b0b91fee2eb04ecab4e
Rutger Spiertz <rutger@kumina.nl>
2012-01-03 13:33:36 +0100

    Grab the external_ipaddress to make sure we use the correct ip.

--- kbp_icinga/manifests/init.pp
+++ kbp_icinga/manifests/init.pp
@@ -1115,7 +1115,7 @@ define kbp_icinga::service($service_description=false, $use=false, $servicegroup
 #
 # Depends:
 #  gen_puppet
-define kbp_icinga::host($conf_dir="${::environment}/${name}",$sms=true,$use=false,$hostgroups=false,$parents=false,$address=$ipaddress,$ensure=present,
+define kbp_icinga::host($conf_dir="${::environment}/${name}",$sms=true,$use=false,$hostgroups=false,$parents=false,$address=$external_ipaddress,$ensure=present,
     $initial_state=false, $notifications_enabled=false, $event_handler_enabled=false, $flap_detection_enabled=false, $process_perf_data=false, $retain_status_information=false,
     $retain_nonstatus_information=false, $check_command=false, $check_interval=false, $notification_period=false, $notification_interval=false, $max_check_attempts=false,
     $register=1, $proxy=false) {
