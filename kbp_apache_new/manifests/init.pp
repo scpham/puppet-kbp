c214c9a35124c4e835b271646ae2b8d4c99e155e
Rutger Spiertz <rutger@kumina.nl>
2012-01-06 15:32:00 +0100

    Prevent vhost_additions for forwarded sites.

--- kbp_apache_new/manifests/init.pp
+++ kbp_apache_new/manifests/init.pp
@@ -288,6 +288,12 @@ define kbp_apache_new::forward_vhost ($forward, $ensure="present", $serveralias=
 }
 
 define kbp_apache_new::vhost_addition($ensure="present", $content=false, $source=false) {
+  $fullname = regsubst($name,'^(.*?)_.*$','\1')
+
+  if defined(Kbp_apache_new::Forward_vhost[$fullname]) {
+    fail("kbp_apache_new::vhost_addition ${name} is inconsistent as a forward is in place for this site.")
+  }
+
   gen_apache::vhost_addition { $name:
     ensure  => $ensure,
     content => $content,
