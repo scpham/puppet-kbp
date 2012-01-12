5aa350d9206efb0f3b3992cb088776dfa05a3e65
Rutger Spiertz <rutger@kumina.nl>
2012-01-12 14:00:34 +0100

    Add wsgi package and module.

--- kbp_django/manifests/init.pp
+++ kbp_django/manifests/init.pp
@@ -1,9 +1,10 @@
 class kbp_django {
   include gen_django
+  include gen_base::libapache2-mod-wsgi
 
   kfile { "/srv/django":
     ensure => directory;
   }
 
-  kbp_apache_new::module { "python":; }
+  kbp_apache_new::module { ["python","wsgi"]:; }
 }
