34a8ebff5e25c323b1d4f7a51168beb66a20e11f
Rutger Spiertz <rutger@kumina.nl>
2012-01-20 09:53:12 +0100

    Use host_name in dependencies.

--- kbp_icinga/manifests/init.pp
+++ kbp_icinga/manifests/init.pp
@@ -986,11 +986,11 @@ define kbp_icinga::service($ensure="present", $service_description=false, $use=f
     }
   }
 
-  if $nrpe {
+  if $nrpe and $register == 1 {
     gen_icinga::servicedependency { "nrpe_dependency_${real_name}_nrpe_port":
       ensure                        => $ensure,
       dependent_service_description => $service_description,
-      host_name                     => $fqdn,
+      host_name                     => $host_name,
       service_description           => "NRPE port",
       execution_failure_criteria    => "c",
       notification_failure_criteria => "c";
