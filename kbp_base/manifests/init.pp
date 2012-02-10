0044bfbda943287c8e24df4ae006a7fd912fae0e
Tim Stoop <tim@kumina.nl>
2012-02-10 10:15:55 +0100

    Curl is part of the base install. Let's keep it up-to-date.

--- kbp_base/manifests/init.pp
+++ kbp_base/manifests/init.pp
@@ -14,6 +14,7 @@ class kbp_base {
   include gen_base::dnsutils
   include gen_base::wget
   include gen_base::telnet_ssl
+  include gen_base::curl
   include gen_cron
   include lvm
   include sysctl
