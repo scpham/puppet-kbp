9b1c45799a66beeb1e59db3e4823fe4484d05fb3
Pieter Lexis <pieter@kumina.nl>
2012-02-21 16:48:09 +0100

    add kbp_java

---
+++ kbp_java/manifests/init.pp
@@ -0,0 +1,11 @@
+class kbp_java::sun_java6_jdk {
+  include gen_java::sun_java6_jdk
+}
+
+class kbp_java::sun_java6_jre_crypto_policy {
+  include gen_java::sun_java6_jre_crypto_policy
+}
+
+class kbp_java::sun_java6_jre {
+  include gen_java::sun_java6_jre
+}
