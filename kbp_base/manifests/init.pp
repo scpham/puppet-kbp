5a273698dbef70a71ae3bc8aab69fe90bb81641e
Rutger Spiertz <rutger@kumina.nl>
2012-04-19 13:55:33 +0200

    Second step in moving staff_user to admin_user.

--- kbp_base/manifests/init.pp
+++ kbp_base/manifests/init.pp
@@ -93,33 +93,33 @@ class kbp_base {
     changes => "set LAST_UID 9999";
   }
 
-  kbp_base::staff_user {
-    "tim":
-      fullname      => "Tim Stoop",
-      uid           => 10001,
-      password_hash => "BOGUS",
-      sshkeys       => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCcRYiKZ1yPUU8+oRaDI/TyRSyFu2fwbknvr/Q3rwbQZm2K8iTfY4/WUeu/oSZOnCn5uoNjGax88RZx92DK0yYpOHtUwG/nShtTwte0Mx4zW8Sfq343OPle2b2gp/0V6dx1Nq21rmQrh0Ql23Thmi33cmKUvPgwYXvsIKfM68J2bG9+hIiucQX0AY7oH8UCX6uJmjOB2nPBsCMAmBHLsfV9LTvSobYAJLEt0m2wV+BqPZW5zLj7HyrGCDa5+85EB4MuQsiYuVdAjQJ3JF/FD0w7LrtuwhKZuS/Qwn4vXah1FlTBlIfw6IxWrQ0+CBCx4h/E4lbxgLTHCB4sanhUGKQtVV1/CFEA9GYCtDbNepFmjuZM1IubarpJmMicOebIW6yT9/035jKuS+nJG2xOLfV4MNPDkuAwqgg1DJ1JmqpG8y1+rHuswbXhlxlfKw/SEooH6I8NDv+TxHSkyo5siacNRsfQ8rQf9fKJdhD0twZuOZU8Zz9wpFz6VCYMkgKp05U= smartcard Tim Stoop\n";
-    "pieter":
-      fullname      => "Pieter Lexis",
-      uid           => 10005,
-      password_hash => "BOGUS",
-      sshkeys       => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCitP9QOGYxCEqueFl/FC+K7o4hjA5zqG+oMirkqxE0EOX6VgwbigvIfzHnUI/LPkDCinhZGnFyfrTkvepcf6Bhtml3ex3lU2HvAiwVfrf/PWeNeg+MUYo7QGpqRUpC+qE82Epe8f0CLpwYo9Bzk4k4Toc1ZMvHUFzSOEBSe9tUetGmP7AGK/WDGJ5hc07XYV1/W3CAGO8XnhIS3/WdDS8D65iOXNQbwrndIfDn2Y3bWfd4qtx+KdY3+LU+6QKPS9Pdl5iYpWw3gln6NMoG8VDCaCNsr9qFNPOD26ninAGkzv31l59xIS94knutgCsov5KdxEWkd49m0ts5L+H9kZ+sznCr4I1Ng3CghABSU9gK7qnQbQR8PlM+eetyzrEFqHYApioyC2xAYJ+D6f65E7WadZBX5DZpqVEYZmw3d3rN49JI9DYlimUKA0Mu7KVfAURZ/3uir3HPZ0tBcyfqKEfT50Nqew92idjnoO191+lyvXXoiAc65EC/y5Ze8ZZ3pfU= pieter@kumina.nl\n";
+#  kbp_base::staff_user {
+#    "tim":
+#      fullname      => "Tim Stoop",
+#      uid           => 10001,
+#      password_hash => "BOGUS",
+#      sshkeys       => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCcRYiKZ1yPUU8+oRaDI/TyRSyFu2fwbknvr/Q3rwbQZm2K8iTfY4/WUeu/oSZOnCn5uoNjGax88RZx92DK0yYpOHtUwG/nShtTwte0Mx4zW8Sfq343OPle2b2gp/0V6dx1Nq21rmQrh0Ql23Thmi33cmKUvPgwYXvsIKfM68J2bG9+hIiucQX0AY7oH8UCX6uJmjOB2nPBsCMAmBHLsfV9LTvSobYAJLEt0m2wV+BqPZW5zLj7HyrGCDa5+85EB4MuQsiYuVdAjQJ3JF/FD0w7LrtuwhKZuS/Qwn4vXah1FlTBlIfw6IxWrQ0+CBCx4h/E4lbxgLTHCB4sanhUGKQtVV1/CFEA9GYCtDbNepFmjuZM1IubarpJmMicOebIW6yT9/035jKuS+nJG2xOLfV4MNPDkuAwqgg1DJ1JmqpG8y1+rHuswbXhlxlfKw/SEooH6I8NDv+TxHSkyo5siacNRsfQ8rQf9fKJdhD0twZuOZU8Zz9wpFz6VCYMkgKp05U= smartcard Tim Stoop\n";
+#    "pieter":
+#      fullname      => "Pieter Lexis",
+#      uid           => 10005,
+#      password_hash => "BOGUS",
+#      sshkeys       => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCitP9QOGYxCEqueFl/FC+K7o4hjA5zqG+oMirkqxE0EOX6VgwbigvIfzHnUI/LPkDCinhZGnFyfrTkvepcf6Bhtml3ex3lU2HvAiwVfrf/PWeNeg+MUYo7QGpqRUpC+qE82Epe8f0CLpwYo9Bzk4k4Toc1ZMvHUFzSOEBSe9tUetGmP7AGK/WDGJ5hc07XYV1/W3CAGO8XnhIS3/WdDS8D65iOXNQbwrndIfDn2Y3bWfd4qtx+KdY3+LU+6QKPS9Pdl5iYpWw3gln6NMoG8VDCaCNsr9qFNPOD26ninAGkzv31l59xIS94knutgCsov5KdxEWkd49m0ts5L+H9kZ+sznCr4I1Ng3CghABSU9gK7qnQbQR8PlM+eetyzrEFqHYApioyC2xAYJ+D6f65E7WadZBX5DZpqVEYZmw3d3rN49JI9DYlimUKA0Mu7KVfAURZ/3uir3HPZ0tBcyfqKEfT50Nqew92idjnoO191+lyvXXoiAc65EC/y5Ze8ZZ3pfU= pieter@kumina.nl\n";
 #    "rutger":
 #      fullname      => "Rutger Spiertz",
 #      uid           => 10003,
 #      password_hash => "BOGUS",
 #      sshkeys       => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCMZBz0sRqmfs4QT4dXVQeMIc+PdDChsjSUQv+SkN//z+igMw6qe5acC8EXUk5CR7VfaOjttp+sgoOxsvFPdFnrcozUsssnUynfVQ4GHCpDu0iOoUtz+WuGGonauAimhFsO2apkYLlO2qipt/z6B+bPQsbOxIVLpLLCa1kFKux7Td4vGddxbCxtFECd/4QUuS42G5q8nET3cdiqHM+QHXs1bnOqa6nxOxhnKX1jlqPT5nwdd8pI+RChGcjD4UofL9IYtz+Nd8wZi/h0tcOUh/ORV1bpJFwTCdWwaQ7Z7bf2Aanzn6iJz14nM0n19EOdvcB5NS/1mE54U9S3qJN+fQT3bOm47R07BIXmCEah6uZUAezkzsnXAsntgn2YDZFhjX+6Xd0iALAlhOyOMVfjJ0cq/qv1WhqScyOOETZhwOjLm4lewigpRnctJBt87p8MArPTBbJJA4TayC9eP6IfZ6plu0Be+W+xvrh/ga3oxMiyg6LWCf2yeTRUut7aIyswxY8= rutger@kumina.nl\n";
-    "thomas":
-      fullname      => "Thomas Ronner",
-      uid           => 10006,
-      password_hash => "BOGUS",
-      sshkeys       => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCSqpjh3qeW8bXvyy1eej+2ief63+4Vr6I7D+cz9UHFXwSA+/q4Aiz5PxVmsTBWvYqo8QQJAlQy4c861WSnMU73Vg9srjBbryGJNHt4DNfnEK2XnOsA246ifwSMXoW2W/cE1gfq6HRR9uL3+ysM9jRYfBvuKuGYb8+3WOdjia6W0CHsHcnhOVVBzfvhekbya9xsROT2cN1/c57JjM+9L3VTiYhLzRBNHKyAkazlpBM6d6A2u+Tq99ovEI2gqrPxsnLqaubGWz1/OdeNCM50pvwi3XCjPJU2jRPUOqpZa0NodOXcpRw44vHAdzwCk3qINWuXhDg0NiXMZ2SU/VDBF4DcjEQiwERyagYStWPuvwcHYw6Yy0GTvESTjZ8eQQg9MrwClo18zcnsirgZSP31Untt5SpvwnVcwZyRrw0i3VVW9nmlxfzbNPXid2VO4SDQ4+8D4zf6ozlumWb1RlafHtKNcI55r3lGoJwKB1NAfTE/cKm1pU1Y5gBBtI85yPCv30k= cardno:000500000C24\n";
-  }
-
-  # Extra configuration for Tim
-  file { "/home/tim/.tmux.conf":
-    content => template("kbp_base/home/tim/.tmux.conf"),
-  }
+#    "thomas":
+#      fullname      => "Thomas Ronner",
+#      uid           => 10006,
+#      password_hash => "BOGUS",
+#      sshkeys       => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCSqpjh3qeW8bXvyy1eej+2ief63+4Vr6I7D+cz9UHFXwSA+/q4Aiz5PxVmsTBWvYqo8QQJAlQy4c861WSnMU73Vg9srjBbryGJNHt4DNfnEK2XnOsA246ifwSMXoW2W/cE1gfq6HRR9uL3+ysM9jRYfBvuKuGYb8+3WOdjia6W0CHsHcnhOVVBzfvhekbya9xsROT2cN1/c57JjM+9L3VTiYhLzRBNHKyAkazlpBM6d6A2u+Tq99ovEI2gqrPxsnLqaubGWz1/OdeNCM50pvwi3XCjPJU2jRPUOqpZa0NodOXcpRw44vHAdzwCk3qINWuXhDg0NiXMZ2SU/VDBF4DcjEQiwERyagYStWPuvwcHYw6Yy0GTvESTjZ8eQQg9MrwClo18zcnsirgZSP31Untt5SpvwnVcwZyRrw0i3VVW9nmlxfzbNPXid2VO4SDQ4+8D4zf6ozlumWb1RlafHtKNcI55r3lGoJwKB1NAfTE/cKm1pU1Y5gBBtI85yPCv30k= cardno:000500000C24\n";
+#  }
+#
+#  # Extra configuration for Tim
+#  file { "/home/tim/.tmux.conf":
+#    content => template("kbp_base/home/tim/.tmux.conf"),
+#  }
 
   # Packages we like and want :)
   kpackage {
@@ -225,74 +225,74 @@ class kbp_base::wanted_packages {
   include gen_base::sysstat
 }
 
-define kbp_base::staff_user($ensure="present", $fullname, $uid, $password_hash, $sshkeys="", $shell="bash") {
-    user { $name:
-      comment      => $fullname,
-      ensure       => $ensure,
-      gid          => "kumina",
-      uid          => $uid,
-      groups       => ["adm", "staff", "root"],
-      membership   => "minimum",
-      shell        => "/bin/${shell}",
-      home         => "/home/${name}",
-      password     => $password_hash,
-      require      => [File["/etc/skel/.bash_profile"], Package[$shell]];
-    }
-
-    if $ensure == "present" {
-      file {
-        "/home/${name}":
-          ensure  => directory,
-          mode    => 750,
-          owner   => $name,
-          group   => "kumina",
-          require => [User[$name], Group["kumina"]];
-        "/home/${name}/.ssh":
-          ensure  => directory,
-          mode    => 700,
-          owner   => $name,
-          group   => "kumina";
-        "/home/${name}/.ssh/authorized_keys":
-          content => $sshkeys,
-          owner   => $name,
-          group   => "kumina",
-          notify  => Kaugeas["sshd_config PermitRootLogin"];
-        "/home/${name}/.${shell}rc":
-          content => template("kbp_base/home/${name}/.${shell}rc"),
-          owner   => $name,
-          group   => "kumina";
-        "/home/${name}/.bash_profile":
-          content => template("kbp_base/home/${name}/.bash_profile"),
-          owner   => $name,
-          group   => "kumina";
-        "/home/${name}/.bash_aliases":
-          content => template("kbp_base/home/${name}/.bash_aliases"),
-          owner   => $name,
-          group   => "kumina";
-        "/home/${name}/.gitconfig":
-          content => template("kbp_base/git/.gitconfig"),
-          group   => "kumina";
-        "/home/${name}/.tmp":
-          ensure  => directory,
-          owner   => $name,
-          group   => "kumina";
-        "/home/${name}/.reportbugrc":
-          content => "REPORTBUGEMAIL=${name}@kumina.nl\n",
-          group   => "kumina";
-      }
-
-      postfix::alias { "${name}: ${name}@kumina.nl":; }
-
-      concat::add_content { "Add ${name} to Kumina SSH keyring":
-        target  => "/etc/ssh/kumina.keys",
-        content => "# ${fullname} <${name}@kumina.nl>\n${sshkeys}";
-      }
-    } else {
-      file { "/home/${name}":
-        ensure  => absent,
-        force   => true,
-        recurse => true;
-      }
-    }
-  }
-
+#define kbp_base::staff_user($ensure="present", $fullname, $uid, $password_hash, $sshkeys="", $shell="bash") {
+#    user { $name:
+#      comment      => $fullname,
+#      ensure       => $ensure,
+#      gid          => "kumina",
+#      uid          => $uid,
+#      groups       => ["adm", "staff", "root"],
+#      membership   => "minimum",
+#      shell        => "/bin/${shell}",
+#      home         => "/home/${name}",
+#      password     => $password_hash,
+#      require      => [File["/etc/skel/.bash_profile"], Package[$shell]];
+#    }
+#
+#    if $ensure == "present" {
+#      file {
+#        "/home/${name}":
+#          ensure  => directory,
+#          mode    => 750,
+#          owner   => $name,
+#          group   => "kumina",
+#          require => [User[$name], Group["kumina"]];
+#        "/home/${name}/.ssh":
+#          ensure  => directory,
+#          mode    => 700,
+#          owner   => $name,
+#          group   => "kumina";
+#        "/home/${name}/.ssh/authorized_keys":
+#          content => $sshkeys,
+#          owner   => $name,
+#          group   => "kumina",
+#          notify  => Kaugeas["sshd_config PermitRootLogin"];
+#        "/home/${name}/.${shell}rc":
+#          content => template("kbp_base/home/${name}/.${shell}rc"),
+#          owner   => $name,
+#          group   => "kumina";
+#        "/home/${name}/.bash_profile":
+#          content => template("kbp_base/home/${name}/.bash_profile"),
+#          owner   => $name,
+#          group   => "kumina";
+#        "/home/${name}/.bash_aliases":
+#          content => template("kbp_base/home/${name}/.bash_aliases"),
+#          owner   => $name,
+#          group   => "kumina";
+#        "/home/${name}/.gitconfig":
+#          content => template("kbp_base/git/.gitconfig"),
+#          group   => "kumina";
+#        "/home/${name}/.tmp":
+#          ensure  => directory,
+#          owner   => $name,
+#          group   => "kumina";
+#        "/home/${name}/.reportbugrc":
+#          content => "REPORTBUGEMAIL=${name}@kumina.nl\n",
+#          group   => "kumina";
+#      }
+#
+#      postfix::alias { "${name}: ${name}@kumina.nl":; }
+#
+#      concat::add_content { "Add ${name} to Kumina SSH keyring":
+#        target  => "/etc/ssh/kumina.keys",
+#        content => "# ${fullname} <${name}@kumina.nl>\n${sshkeys}";
+#      }
+#    } else {
+#      file { "/home/${name}":
+#        ensure  => absent,
+#        force   => true,
+#        recurse => true;
+#      }
+#    }
+#  }
+#
