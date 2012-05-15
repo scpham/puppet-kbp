3739d6e476f6fa60f675c1ef570d6d25e379aa58
Pieter Lexis <pieter@kumina.nl>
2012-05-15 10:19:32 +0200

    remove acpid pinnings

--- kbp_acpi/manifests/init.pp
+++ kbp_acpi/manifests/init.pp
@@ -11,18 +11,4 @@
 #
 class kbp_acpi {
   include gen_acpi
-
-  if $lsbmajdistrelease < 6 {
-    gen_apt::preference {
-      ["acpid"]:
-        version => "1.0.8-1lenny4";
-    }
-  }
-  else {
-    gen_apt::preference {
-      ["acpid"]:
-        version => "1:2.0.7-1squeeze3";
-    }
-  }
-
 }
