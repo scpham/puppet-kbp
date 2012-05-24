aec9a01b8b325e4fcdb79f03c4d563d7725767c5
Pieter Lexis <pieter@kumina.nl>
2012-05-24 12:45:50 +0200

    javaheap check used a wrong argument, hopefully this fixes it

--- kbp_icinga/manifests/init.pp
+++ kbp_icinga/manifests/init.pp
@@ -1673,7 +1673,7 @@ define kbp_icinga::haproxy($address, $ha=false, $url=false, $port=false, $host_n
 #  gen_puppet
 #
 define kbp_icinga::java($servicegroups=false, $sms=true, $username=false, $password=false) {
-  $jmx_port = regsubst($name,^[^_]*_,"")
+  $jmx_port = regsubst($name, '^[^_]*_', '')
 
   kbp_icinga::service { "java_heap_usage_${name}":
     service_description => "Java heap usage ${name}",
