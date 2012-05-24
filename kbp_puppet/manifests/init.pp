d71ab828591014f30e0e3c0e983ee5847217ddc9
Tim Stoop <tim@kumina.nl>
2012-05-24 09:22:41 +0200

    Follow styleguide.

--- kbp_puppet/manifests/init.pp
+++ kbp_puppet/manifests/init.pp
@@ -90,7 +90,7 @@ class kbp_puppet::default_config {
 
   kcron { "run-puppet":
     mailto  => "reports@kumina.nl",
-    command => "/bin/sleep $sleep; /usr/bin/test ! -f /etc/puppet/dontrunpuppetd && /usr/bin/puppet agent --onetime --no-daemonize --no-splay --color false --logdest console --logdest syslog > /dev/null",
+    command => "/bin/sleep ${sleep}; /usr/bin/test ! -f /etc/puppet/dontrunpuppetd && /usr/bin/puppet agent --onetime --no-daemonize --no-splay --color false --logdest console --logdest syslog > /dev/null",
     hour    => $hours,
     minute  => "0",
   }
