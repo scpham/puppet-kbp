6fa4fe1cc7708c96c790925871a0d9d562ccd71b
Rutger Spiertz <rutger@kumina.nl>
2012-06-25 20:42:55 +0200

    Only set address when needed.

--- kbp_munin/manifests/init.pp
+++ kbp_munin/manifests/init.pp
@@ -604,7 +604,6 @@ class kbp_munin::two::server ($site, $wildcard=false, $intermediate=false, $use_
   }
 
   kbp_apache_new::site { $site:
-    address      => $ipaddress,
     make_default => true,
     wildcard     => $wildcard,
     intermediate => $intermediate;
