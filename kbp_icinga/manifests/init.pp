ee1ded22964944b8e577eec7722a397ac2c997dc
Rutger Spiertz <rutger@kumina.nl>
2012-08-10 08:23:21 +0200

    Fix scoping.

--- kbp_icinga/manifests/init.pp
+++ kbp_icinga/manifests/init.pp
@@ -1413,7 +1413,7 @@ define kbp_icinga::service($ensure="present", $service_description=false, $use=f
 
 define kbp_icinga::servicedependency($ensure="present", $dependent_service_description, $host_name=$fqdn, $service_description, $conf_dir="${environment}/${fqdn}", $dependent_host_name=$fqdn,
     $execution_failure_criteria=false, $notification_failure_criteria="o") {
-  if $::monitoring == 'true' or ($override_nomonitoring and $::monitoring != 'force_off') {
+  if $::monitoring == 'true' or ($::override_nomonitoring and $::monitoring != 'force_off') {
     gen_icinga::servicedependency { $name:
       ensure                        => $ensure,
       dependent_service_description => $dependent_service_description,
