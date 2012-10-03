39f50bd83f611c2fff15e103ae9c981ec1ee80ed
Rutger Spiertz <rutger@kumina.nl>
2012-10-03 11:55:47 +0200

    Rename param.

--- kbp_haproxy/manifests/init.pp
+++ kbp_haproxy/manifests/init.pp
@@ -121,7 +121,7 @@ define kbp_haproxy::site ($listenaddress, $port=80, $monitor_site=true, $monitor
         port                 => $real_sslport,
         max_check_attempts   => $max_check_attempts,
         response             => $monitoring_response,
-        nrpe_proxy           => $monitoring_proxy,
+        proxy                => $monitoring_proxy,
         preventproxyoverride => true;
       }
     }
@@ -138,7 +138,7 @@ define kbp_haproxy::site ($listenaddress, $port=80, $monitor_site=true, $monitor
       port                 => $port,
       max_check_attempts   => $max_check_attempts,
       response             => $monitoring_response,
-      nrpe_proxy           => $monitoring_proxy,
+      proxy                => $monitoring_proxy,
       preventproxyoverride => true;
     }
   }
