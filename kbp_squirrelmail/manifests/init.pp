33baaab0696deb0c00609ef2b8b3fb6d0c668407
Thomas Ronner <thomas@kumina.nl>
2012-10-10 12:29:25 +0200

    Remove ssl options for squirrelmail site and fix javascript issue with avelsieve.

--- kbp_squirrelmail/manifests/init.pp
+++ kbp_squirrelmail/manifests/init.pp
@@ -3,13 +3,11 @@ class kbp_squirrelmail {
   include gen_squirrelmail::plugin::avelsieve
 }
 
-define kbp_squirrelmail::site ($ssl=false, $intermediate=false, $redirect_non_ssl=false, $address='*', $serveralias=false, $org_name=false, $org_logo=false, $org_logo_width=false, $org_logo_height=false, $org_title=false, $provider_uri=false) {
+define kbp_squirrelmail::site ($intermediate=false, $address='*', $serveralias=false, $org_name=false, $org_logo=false, $org_logo_width=false, $org_logo_height=false, $org_title=false, $provider_uri=false) {
   include kbp_squirrelmail
 
   kbp_apache::site { $name:
-    ssl                 => $ssl,
     intermediate        => $intermediate,
-    redirect_non_ssl    => $redirect_non_ssl,
     address             => $address,
     serveralias         => $serveralias,
     documentroot        => '/usr/share/squirrelmail',
@@ -17,9 +15,13 @@ define kbp_squirrelmail::site ($ssl=false, $intermediate=false, $redirect_non_ss
     php                 => true;
   }
 
-  file {
-    "/etc/squirrelmail/config_local.php":
-      content => template("kbp_squirrelmail/config_local.php"),
-      require => Package["squirrelmail"];
+  file { "/etc/squirrelmail/config_local.php":
+    content => template("kbp_squirrelmail/config_local.php"),
+    require => Package["squirrelmail"];
+  }
+
+  kbp_apache::vhost_addition { "${name}/javascript":
+    ports    => '443',
+    content  => "Alias /javascript /usr/share/javascript\n";
   }
 }
