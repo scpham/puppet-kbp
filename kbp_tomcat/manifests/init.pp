742b482a711600ab875c76686f7c2645f4c02bab
Tim Stoop <tim@kumina.nl>
2012-10-25 14:42:32 +0200

    Allow wildcard cert as well!

--- kbp_tomcat/manifests/init.pp
+++ kbp_tomcat/manifests/init.pp
@@ -242,7 +242,7 @@ define kbp_tomcat::datasource_setting ($context, $hash) {
 #  gen_puppet
 #
 define kbp_tomcat::apache_proxy_ajp_site($ensure="present", $port=8009, $ssl=false, $address='*', $redirect_non_ssl=true, $non_ssl=true, $serveralias=false, $documentroot="/srv/www/${name}", $tomcat_tag="tomcat_${environment}",
-    $sourcepath="/", $urlpath="/", $php=false, $make_default=false, $monitor_path=$urlpath, $monitor_response=false, $monitor_max_check_attempts="5", $auth=false, $intermediate=false, $cert=false) {
+    $sourcepath="/", $urlpath="/", $php=false, $make_default=false, $monitor_path=$urlpath, $monitor_response=false, $monitor_max_check_attempts="5", $auth=false, $intermediate=false, $cert=false, $wildcard=false) {
   if $ssl or $intermediate {
     $real_ssl = true
   } else {
@@ -264,6 +264,7 @@ define kbp_tomcat::apache_proxy_ajp_site($ensure="present", $port=8009, $ssl=fal
     redirect_non_ssl   => $redirect_non_ssl,
     non_ssl            => $non_ssl,
     cert               => $cert,
+    wildcard           => $wildcard,
     intermediate       => $intermediate,
     require            => Kbp_apache::Module["proxy_ajp"];
   }
