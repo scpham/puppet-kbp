bb1570a8cf2ebd58962cc784d3c5958d91638127
Rutger Spiertz <rutger@kumina.nl>
2012-11-07 12:53:45 +0100

    Add monitoring param.

--- kbp_squirrelmail/manifests/init.pp
+++ kbp_squirrelmail/manifests/init.pp
@@ -3,7 +3,7 @@ class kbp_squirrelmail {
   include gen_squirrelmail::plugin::avelsieve
 }
 
-define kbp_squirrelmail::site ($intermediate=false, $address='*', $serveralias=false, $org_name=false, $org_logo=false, $org_logo_width=false, $org_logo_height=false, $org_title=false, $provider_uri=false) {
+define kbp_squirrelmail::site ($intermediate=false, $address='*', $serveralias=false, $org_name=false, $org_logo=false, $org_logo_width=false, $org_logo_height=false, $org_title=false, $provider_uri=false, $monitor_path=false) {
   include kbp_squirrelmail
 
   kbp_apache::site { $name:
@@ -12,7 +12,8 @@ define kbp_squirrelmail::site ($intermediate=false, $address='*', $serveralias=f
     address      => $address,
     serveralias  => $serveralias,
     documentroot => '/usr/share/squirrelmail',
-    php          => true;
+    php          => true,
+    monitor_path => $monitor_path;
   }
 
   file { "/etc/squirrelmail/config_local.php":
