dd85d104592b54577f1e6505d17a95dc59653235
Rutger Spiertz <rutger@kumina.nl>
2012-11-12 14:44:07 +0100

    Kbp_ocfs2 rewrite.

--- kbp_ocfs2/manifests/init.pp
+++ kbp_ocfs2/manifests/init.pp
@@ -13,43 +13,40 @@
 #  Undocumented
 #  gen_puppet
 #
-class kbp_ocfs2($ocfs2_tag="", $use_ipaddress=$external_ipaddress) {
+class kbp_ocfs2($ocfs2_tag=false, $use_ipaddress=$external_ipaddress) {
   include gen_ocfs2
   include gen_base::libcups2
 
-  $real_tag = "ferm_ocfs2_${environment}_${ocfs2_tag}"
-
-  concat { "/etc/ocfs2/cluster.conf":
-    notify => Kservice["o2cb"];
+  $real_tag = $ocfs2_tag ? {
+    false   => "ocfs2_${environment}_${custenv}",
+    default => "ocfs2_${environment}_${custenv}_${ocfs2_tag}",
   }
 
-  concat::add_content { "Ocfs2 cluster basic config":
-    content => template("kbp_ocfs2/basic"),
-    target  => "/etc/ocfs2/cluster.conf";
+  concat { '/etc/ocfs2/cluster.conf':
+    notify => Kservice['o2cb'];
   }
 
-  kbp_ocfs2::cluster_config { "cluster_config":
-    use_ipaddress => $use_ipaddress,
-    real_tag      => $real_tag;
+  concat::add_content { 'ocfs2':
+    content => template('kbp_ocfs2/basic'),
+    target  => '/etc/ocfs2/cluster.conf';
+  }
+
+  @@concat::add_content { "ocfs2_${fqdn}":
+    content => template('kbp_ocfs2/node'),
+    target  => '/etc/ocfs2/cluster.conf',
+    tag     => $real_tag;
   }
 
   Concat::Add_content <<| tag == $real_tag |>>
 
-  Gen_ferm::Rule <<| tag == $real_tag |>>
-
-  @@gen_ferm::rule { "OCFS2 connections from ${fqdn}":
-    saddr  => $use_ipaddress,
-    proto  => "tcp",
-    dport  => 7777,
-    action => "ACCEPT",
-    tag    => $real_tag;
+  kbp_ferm::rule { 'OCFS2 connections':
+    saddr    => $use_ipaddress,
+    proto    => 'tcp',
+    dport    => 7777,
+    action   => 'ACCEPT',
+    exported => true,
+    ferm_tag => $real_tag;
   }
-}
 
-define kbp_ocfs2::cluster_config($real_tag, $fqdn=$fqdn, $use_ipaddress=$external_ipaddress) {
-  @@concat::add_content { "Ocfs2 cluster config for ${fqdn}":
-    content => template("kbp_ocfs2/node"),
-    target  => "/etc/ocfs2/cluster.conf",
-    tag     => $real_tag;
-  }
+  Kbp_ferm::Rule <<| tag == $real_tag |>>
 }
