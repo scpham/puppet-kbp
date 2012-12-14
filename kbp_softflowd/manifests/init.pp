71dca7a1bbd0044daa45007af8bf32e268d69edc
Thomas Ronner <thomas@kumina.nl>
2012-12-14 10:16:44 +0100

    Add maxflows parameter.

--- kbp_softflowd/manifests/init.pp
+++ kbp_softflowd/manifests/init.pp
@@ -12,13 +12,14 @@
 #   version    Netflow version (default '9')
 #   maxlife    Max lifetime of a flow (default '5m' (= five minutes))
 #   expint     Flow expire interval, expint=0 means expire immediately (default '0')
+#   maxflows   Maximum flows concurrently tracked (softflowd's default is 8192)
 #
 # Depends:
 #  gen_puppet
 #  gen_softflowd
 #  kbp_icinga
 #
-class kbp_softflowd ($interface='eth0', $host='127.0.0.1', $port='9995', $version='9', $maxlife='5m', $expint='0') {
+class kbp_softflowd ($interface='eth0', $host='127.0.0.1', $port='9995', $version='9', $maxlife='5m', $expint='0', $maxflows=false) {
   $nf_collector = "${host}:${port}"
 
   class { 'gen_softflowd':
@@ -27,11 +28,12 @@ class kbp_softflowd ($interface='eth0', $host='127.0.0.1', $port='9995', $versio
     port      => $port,
     version   => $version,
     maxlife   => $maxlife,
-    expint    => $expint;
+    expint    => $expint,
+    maxflows  => $maxflows;
   }
 
   kbp_icinga::service { "softflowd":
-    service_description => "softflowd daemon",
+    service_description => "softflowd process",
     check_command       => "check_softflowd",
     nrpe                => true;
   }
