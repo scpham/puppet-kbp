63bb186e78bc75f79ca88a2343dcfbbaa798099d
Pieter Lexis <pieter@kumina.nl>
2013-01-11 13:45:42 +0100

    kbp_dashboard: fix the all_ipaddresses fact for rutger

--- kbp_dashboard/lib/facter/all_ipaddresses.rb
+++ kbp_dashboard/lib/facter/all_ipaddresses.rb
@@ -1,10 +1,16 @@
 fact = ''
 Facter.value(:interfaces).split(',').each do |interface|
   fact = fact + interface + '|'
-  %x{/bin/ip address show #{interface}}.scan(/^    inet6? ([^ ]*)/).flatten.each do |address|
+  addresses = %x{/bin/ip address show #{interface}}
+  addresses.scan(/^    inet ([^ ]*)/).flatten.each do |address|
+    fact = fact + address + ','
+  end
+  fact.chomp!(',')
+  fact = fact + '|'
+  addresses.scan(/^    inet6 ([^ ]*)/).flatten.each do |address|
     fact = fact + address + ','
   end
   fact.chomp!(',')
   fact = fact + ';'
 end
-Facter.add(:all_ipaddress) { setcode { fact } }
+Facter.add(:all_ipaddress) { setcode { fact.chomp(';') } }
