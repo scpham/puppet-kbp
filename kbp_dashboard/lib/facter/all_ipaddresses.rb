dfa1b38fce01d5e3b106e2c16be933a44a104e02
Pieter Lexis <pieter@kumina.nl>
2013-01-23 14:35:29 +0100

    Have all_ipaddresses ignore link-local and loopback addresses

--- kbp_dashboard/lib/facter/all_ipaddresses.rb
+++ kbp_dashboard/lib/facter/all_ipaddresses.rb
@@ -3,12 +3,12 @@ Facter.value(:interfaces).split(',').each do |interface|
   fact = fact + interface + '|'
   addresses = %x{/bin/ip address show #{interface}}
   addresses.scan(/^    inet ([^ ]*)/).flatten.each do |address|
-    fact = fact + address + ','
+    fact = fact + address + ',' if address !~ /^127\.0\.0\./
   end
   fact.chomp!(',')
   fact = fact + '|'
   addresses.scan(/^    inet6 ([^ ]*)/).flatten.each do |address|
-    fact = fact + address + ','
+    fact = fact + address + ',' if address !~ /^fe80:|^::1/
   end
   fact.chomp!(',')
   fact = fact + ';'
