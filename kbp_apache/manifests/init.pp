76aff97bffc127f85e14241eb0005b5f017e4e7c
Tim Stoop <tim@kumina.nl>
2013-02-14 18:07:41 +0100

    Do not backup Apache's running state.

--- kbp_apache/manifests/init.pp
+++ kbp_apache/manifests/init.pp
@@ -59,6 +59,9 @@ class kbp_apache {
   kbp_apache::module { ["deflate","rewrite"]:; }
 
   kbp_icinga::http { "http_${fqdn}":; }
+
+  # Do not backup running state of Apache
+  kbp_backup::exclude { '/var/lib/apache2':; }
 }
 
 # Class: kbp_apache::global_umask_007
