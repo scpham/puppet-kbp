e6ed60897d8ad18e3a33c8fad5246e4f9f479ced
Pieter Lexis <pieter@kumina.nl>
2013-02-25 10:05:25 +0100

    Add group options to the wp_fix_permissions script

--- kbp_wordpress/templates/fix_wp_permissions.sh
+++ kbp_wordpress/templates/fix_wp_permissions.sh
@@ -11,13 +11,15 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 usage() {
-	echo "Usage: $0 <PATH|all>" >&2
+	echo "Usage: $0 [-g|--group GROUP] <PATH|all>" >&2
 	echo "  PATH is the path to the directory containing the WordPress" >&2
 	echo "  installation. e.g. '/srv/www/myblog.mysite.com'." >&2
 	echo "  When supplying 'all', ALL subdirectories of /srv/www are checked" >&2
 	echo "  for WordPress installations and all wp-content/uploads and " >&2
 	echo "  wp-content/blogs.dir directories and file therein will be " >&2
 	echo "  chowned to www-data." >&2
+	echo "" >&2
+	echo "  GROUP is an optional group name to chgrp the WP installation to." >&2
 	exit 1
 }
 
@@ -27,12 +29,22 @@ do_chown() {
 		/srv/www/*)
 			if [ -f $P/wp-config.php -a -d $P/wp-content/uploads ]; then # This is a WordPress directory
 				/usr/bin/find -P $P/wp-content/uploads \( -type d -or -type f \) -not -user www-data -exec /bin/chown -h www-data {} +
-				/usr/bin/find -P $P/wp-content/uploads -type d -not -perm -u=rwx -exec /bin/chown u=rwx {} +
+				/usr/bin/find -P $P/wp-content/uploads -type d -not -perm -u=rwx -exec /bin/chmod u=rwx {} +
 				/usr/bin/find -P $P/wp-content/uploads -type f -not -perm -u=rw -exec /bin/chmod u=rw {} +
+				if [ ! -z $GROUP ]; then
+					/usr/bin/find -P $P/wp-content/uploads \( -type d -or -type f \) -not -group $GROUP -exec /bin/chgrp -h $GROUP {} +
+					/usr/bin/find -P $P/wp-content/uploads -type d -not -perm -g=rwx -exec /bin/chmod g=rwx {} +
+					/usr/bin/find -P $P/wp-content/uploads -type f -not -perm -g=rw -exec /bin/chmod g=rw {} +
+				fi
 				if [ -d $P/wp-content/blogs.dir ]; then # We also have multi-site
 					/usr/bin/find -P $P/wp-content/blogs.dir \( -type d -or -type f \) -not -user www-data -exec /bin/chown -h www-data {} +
 					/usr/bin/find -P $P/wp-content/blogs.dir -type d -not -perm -u=rwx -exec /bin/chmod u=rwx {} +
 					/usr/bin/find -P $P/wp-content/blogs.dir -type f -not -perm -u=rw -exec /bin/chmod u=rw {} +
+					if [ ! -z $GROUP ]; then
+						/usr/bin/find -P $P/wp-content/blogs.dir \( -type d -or -type f \) -not -group $GROUP -exec /bin/chgrp -h $GROUP {} +
+						/usr/bin/find -P $P/wp-content/blogs.dir -type d -not -perm -g=rwx -exec /bin/chmod g=rwx {} +
+						/usr/bin/find -P $P/wp-content/blogs.dir -type f -not -perm -g=rw -exec /bin/chmod g=rw {} +
+					fi
 				fi
 			else
 				if [ $2 ]; then # if the dir is not a WP directory AND it was supplied by the user
@@ -42,19 +54,47 @@ do_chown() {
 			fi
 			;;
 		*)
-			echo "Error: $1 is not a subdirectory of /srv/www, refusing to continue." >&2
+			echo "Error: ${1} is not a subdirectory of /srv/www, refusing to continue." >&2
 			echo "Please contact support@kumina.nl if you want to change permissions on files." >&2
 			exit 3
 			;;
 	esac
 }
 
+# Have getopt parse the arguments
+ARGS=`/usr/bin/getopt -o g:hu -l group:,help,usage -n $0 -- "$@"`
+
+if [ $? -ne 0 ]; then # If something went wrong
+	echo "" >&2
+	usage
+fi
+
+eval set -- "$ARGS"
+
+GROUP=''
+while true; do
+	case "$1" in
+		-g|--group) GROUP=$2; shift 2;;
+		-h|--help|-u|--usage) usage;;
+		--) shift; break;;
+		*) exit 1;;
+	esac
+done
+
+# is a directory supplied?
 if [ -z $1 ]; then
 	usage
-elif [ $# -ne 1 ]; then
-	usage
-elif [ $1 = '-h' -o $1 = '--help' -o $1 = '-u' -o $1 = '--usage' ]; then
-	usage
+fi
+
+# check if the user is indeed in the group
+if [ ! -z $GROUP ]; then
+	# XXX this code assumes we're run using sudo
+	if $(id -G -n $SUDO_USER | grep -v -q $GROUP); then # The user isn't in the group $GROUP
+		if $(id -G -n $SUDO_USER | grep -v -q kumina); then # The user is also not in the group kumina
+			echo "You (${SUDO_USER}) are not in the group ${GROUP}, refusing to continue." >&2
+			exit 4
+		fi
+	fi
 fi
 
 if [ $1 = 'all' ]; then
@@ -64,10 +104,10 @@ if [ $1 = 'all' ]; then
 	exit 0
 else
 	if [ -d $1 ]; then
-		do_chown $1 X
+		do_chown $1
 		exit 0
 	else
 		echo "Error: ${1} is not a directory." >&2
-		exit 4
+		exit 5
 	fi
 fi
